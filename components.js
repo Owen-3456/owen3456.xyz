362f4c8341830fad23b0ef44bc3857ed8b5c8a7f
Owen <78411938+Owen-3456@users.noreply.github.com>
2024-09-03 14:24:52 +0100

    Removed pipe between navbar items

--- components.js
+++ components.js
@@ -10,9 +10,7 @@ header.innerHTML = `
     <a class="nav-title" href="/">Owen3456</a>
     <ul class="nav-links">
         <li><a href="/">Home</a></li>
-        <li>|</li>
         <li><a href="contact.html">Contact</a></li>
-        <li>|</li>
         <li><a href="projects.html">Projects</a></li>
     </ul>
     <ul class="nav-social">
