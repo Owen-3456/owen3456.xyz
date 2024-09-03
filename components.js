85cd759948ac16a931a024479372a0bd79a67c3f
Owen <78411938+Owen-3456@users.noreply.github.com>
2024-09-03 02:30:15 +0100

    Added projects page

--- components.js
+++ components.js
@@ -12,6 +12,8 @@ header.innerHTML = `
         <li><a href="/">Home</a></li>
         <li>|</li>
         <li><a href="contact.html">Contact</a></li>
+        <li>|</li>
+        <li><a href="projects.html">Projects</a></li>
     </ul>
     <ul class="nav-social">
         <li>
